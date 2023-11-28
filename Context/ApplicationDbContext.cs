e1c8fee0444174a6b81c4da79f0482767ffb0486
yunusemre.aydin <yunusemre.aydin@solmaz.com>
2023-11-28 05:50:03 +0300

    first commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProject.Models;
+
+namespace TestProject.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string EmaillAddress { get; set; } = null!;
+
+    }
+}
